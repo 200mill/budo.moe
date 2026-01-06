3a12b5d90da12ec642b68cc4107457df59c6581e
minco <mail@drchi.co.kr>
2026-01-07 03:01:59 +0900

    feat: migrate to eto.pet

--- ruma-moe-frontend/scripts/push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . -t ruma-moe-frontend
-docker image tag ruma-moe-frontend:latest registry.walruslab.org/pub/ruma-moe-frontend:latest
-docker push registry.walruslab.org/pub/ruma-moe-frontend:latest
-
